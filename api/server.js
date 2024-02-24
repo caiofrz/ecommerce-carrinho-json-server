09a272769a8935647f87809ac910e27b6a61c1d2
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-02-24 17:05:25 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -14,9 +14,6 @@ const server = jsonServer.create()
 // Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
-const middlewares = jsonServer.defaults()
-
-server.use(middlewares)
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
