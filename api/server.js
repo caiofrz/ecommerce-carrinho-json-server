f0773c20d5474a2f76bd0f73398357d6364b463b
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2024-02-24 17:04:09 -0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -17,6 +17,12 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
